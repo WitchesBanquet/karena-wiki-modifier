b15954337cb368fe8f8c78b513db7fd9a4639bc7
Liam Sho <conan_creeper@outlook.com>
2022-04-23 01:52:52 +0800

    fix.Fix dev comment

--- push-docker.sh
+++ push-docker.sh
@@ -8,32 +8,32 @@ VERSION_PATCH="${VERSION##*.}"
 # Single Image
 docker image tag wiki-js-karena-modified:${VERSION}-amd64 $2/wiki-js-karena-modified:${VERSION}-amd64
 docker image tag wiki-js-karena-modified:${VERSION}-arm64 $2/wiki-js-karena-modified:${VERSION}-arm64
-# docker image push --all-tags $2/wiki-js-karena-modified
+docker image push --all-tags $2/wiki-js-karena-modified
 
 # {MAJOR}.{MINOR}.{PATCH} Manifest
 docker manifest create $2/wiki-js-karena-modified:${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH} \
     $2/wiki-js-karena-modified:${VERSION}-amd64 \
     $2/wiki-js-karena-modified:${VERSION}-arm64
 
-#docker manifest push $2/wiki-js-karena-modified:${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}
+docker manifest push $2/wiki-js-karena-modified:${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}
 
 # {MAJOR}.{MINOR} Manifest
 docker manifest create $2/wiki-js-karena-modified:${VERSION_MAJOR}.${VERSION_MINOR} \
     $2/wiki-js-karena-modified:${VERSION}-amd64 \
     $2/wiki-js-karena-modified:${VERSION}-arm64
 
-#docker manifest push $2/wiki-js-karena-modified:${VERSION_MAJOR}.${VERSION_MINOR}
+docker manifest push $2/wiki-js-karena-modified:${VERSION_MAJOR}.${VERSION_MINOR}
 
 # {MAJOR} Manifest
 docker manifest create $2/wiki-js-karena-modified:${VERSION_MAJOR} \
     $2/wiki-js-karena-modified:${VERSION}-amd64 \
     $2/wiki-js-karena-modified:${VERSION}-arm64
 
-#docker manifest push $2/wiki-js-karena-modified:${VERSION_MAJOR}
+docker manifest push $2/wiki-js-karena-modified:${VERSION_MAJOR}
 
 # Latest Manifest
 docker manifest create --amend $2/wiki-js-karena-modified:latest \
     $2/wiki-js-karena-modified:${VERSION}-amd64 \
     $2/wiki-js-karena-modified:${VERSION}-arm64
 
-#docker manifest push $2/wiki-js-karena-modified:latest
+docker manifest push $2/wiki-js-karena-modified:latest
